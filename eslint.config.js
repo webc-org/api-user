7159a8892fb7c63f8c7536a0d184901db7a5d523
charles olivier <chla84@yahoo.fr>
2025-03-20 18:04:54 +0100

    no message

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,ts,jsx,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    rules: {
+      // Your custom rules here
+    },
+  },
+];
