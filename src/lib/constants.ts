95542edb526519c733d3da15de81e5379e83d09c
charles olivier <chla84@yahoo.fr>
2025-03-20 13:08:00 +0100

    no message

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,5 +3,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY || "";
-export const MONGO_URI: string = process.env.MONGO_DB ?? "";
+export const MONGO_URI: string = process.env.MONGO_URI ?? "";
 export const PORT: number = parseInt(process.env.PORT as string) || 8000;
