a4edf194038c3ac4c860ca8c0160b965d8db730b
charles olivier <chla84@yahoo.fr>
2025-03-20 10:52:45 +0100

    no message

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,8 @@ import Server from "./server";
 import { PORT } from "./lib/constants";
 
 const server = new Server();
+
 server.start(PORT);
-
-console.log("Starting server...");
-
-process.on("exit", (code) => {
-  console.log(`Process exiting with code: ${code}`);
-});
-
-process.on("uncaughtException", (err) => {
-  console.error("Uncaught exception:", err);
-});
-
-process.on("unhandledRejection", (reason, promise) => {
-  console.error("Unhandled Rejection at:", promise, "reason:", reason);
-});
 
 process.on("SIGTERM", () => {
   server.stop();
